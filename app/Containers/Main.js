f700bd17cd5304783a0fccbb4c850f9fb03b2ff4
drewminns <drew.minns@me.com>
2018-08-04 07:55:17 -0400

    Added Normalize and testing deployment process

--- app/Containers/Main.js
+++ app/Containers/Main.js
@@ -1,8 +1,13 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
 export default class Main extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  state = {};
+
   render() {
     return <div> Hello world </div>;
   }
