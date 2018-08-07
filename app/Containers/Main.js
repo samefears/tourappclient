943189c892288586bbd4361cc76135de437bc47a
Drew Minns <drew.minns@me.com>
2018-08-07 12:46:13 -0400

    Update Main.js

--- app/Containers/Main.js
+++ app/Containers/Main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, {
   Component
 } from 'react';
@@ -6,10 +6,7 @@ import { Route, Link, Switch } from 'react-router-dom';
 import Tours from './Tours';
 import Chat from './Chat';
 import Nav from './Nav';
-=======
-import React, { Component } from 'react';
 import propTypes from 'prop-types';
->>>>>>> d9f87b2a1235cc183caa265041737b0f9bc83b3d
 
 export default class Main extends Component {
   // static propTypes = {};
